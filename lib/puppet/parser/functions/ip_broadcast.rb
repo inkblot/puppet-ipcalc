1205f9c72302bb5e05571bd679052ea21c07034d
Nate Riffe <inkblot@movealong.org>
2012-09-04 22:07:24 -0500

    Modify ip_broadcast and ip_network
    This changes to behavior of these two functions so that they return a value
    which continues to carry the prefix length.  To turn an offset address into a
    simple IP network address, without a prefix, the new way is to chain the functions:
    
    ip_address(ip_network("10.10.10.10/19")) == "10.10.8.0"
    
    Likewise for ip_broadcast.

--- lib/puppet/parser/functions/ip_broadcast.rb
+++ lib/puppet/parser/functions/ip_broadcast.rb
@@ -5,9 +5,9 @@ module Puppet::Parser::Functions
 	newfunction(:ip_broadcast, :type => :rvalue) do |args|
 		case args.length
 		when 1
-			IP.new(args[0]).broadcast.to_addr
+			IP.new(args[0]).broadcast
 		when 2
-			IP.new(args[0]).broadcast(args[1]).to_addr
+			IP.new(args[0]).broadcast(args[1])
 		end
 	end
 

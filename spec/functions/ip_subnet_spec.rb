13e3318d04b9082fd9df0979c2db51095cee9d52
Mateusz Kijowski <mateusz@kijowski.info>
2017-01-26 20:43:55 +0100

    negative prefix length tests, just in case

--- spec/functions/ip_subnet_spec.rb
+++ spec/functions/ip_subnet_spec.rb
@@ -4,6 +4,7 @@ describe 'ip_subnet' do
     it { should run.with_params('10.0.0.1/23',24,1).and_return("10.0.1.0/24") }
     it { should run.with_params('10.0.0.1/255.255.254.0',24,0).and_return("10.0.0.0/24") }
     it { should run.with_params('10.0.0.1/23',24,2).and_raise_error(ArgumentError) }
+    it { should run.with_params('10.0.0.1/23',-1,2).and_raise_error(ArgumentError) }
     it { should run.with_params('10.0.0.1/23',23,1).and_raise_error(ArgumentError) }
     it { should run.with_params('10.0.0.1/23',22,1).and_raise_error(ArgumentError) }
     it { should run.with_params('10.0.0.1/23',24,2).and_raise_error(ArgumentError) }
--- spec/functions/ip_supernet_spec.rb
+++ spec/functions/ip_supernet_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'ip_supernet' do
     it { should run.with_params('10.0.0.1/23',22).and_return("10.0.0.0/22") }
     it { should run.with_params('10.0.0.1/23',24).and_raise_error(ArgumentError) }
+    it { should run.with_params('10.0.0.1/23',-1).and_raise_error(ArgumentError) }
     it { should run.with_params('10.0.0.1/255.255.254.0',22).and_return("10.0.0.0/22") }
     it { should run.with_params('10.0.0.1/255.255.254.0',24).and_raise_error(ArgumentError) }
     it { should run.with_params('10.0.0.1/255.255.255.256',32).and_raise_error(IPAddr::InvalidAddressError) }
